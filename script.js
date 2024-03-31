0ba6b820d1f5ac87a5f61f9cd5bfe1fc76d889da
Monaflemit <108365633+Monaflemit@users.noreply.github.com>
2024-03-31 11:25:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*let table = document.getElementsByTagName('tr'); // Liste de tr (donc chaque ligne de ton tableau sauf la toute permière qui est les infos)
+console.log(table);
+// On boucle chaque ligne
+for (let i = 0; i < table.length; i++) {
+    let color = table[i][1].innerHTML; // On récupère le texte dans la deuxième colone (type)
+    table[i][1].style.color = color; // On ajoute la coleur dans le type
+};*/
+
+
